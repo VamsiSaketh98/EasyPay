f7671cefd1ffe7cdf28b04f779274d0697db2bf9
vamsi saketh <vamsisaketh59@gmail.com>
2024-05-27 12:06:45 +0530

    bug fix

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -21,13 +21,13 @@ export const Signup = () => {
         <SubHeading label={"Enter your infromation to create an account"} />
         <InputBox onChange={e => {
           setFirstName(e.target.value);
-        }} placeholder="John" label={"First Name"} />
+        }} placeholder="james" label={"First Name"} />
         <InputBox onChange={(e) => {
           setLastName(e.target.value);
-        }} placeholder="Doe" label={"Last Name"} />
+        }} placeholder="bond" label={"Last Name"} />
         <InputBox onChange={e => {
           setUsername(e.target.value);
-        }} placeholder="harkirat@gmail.com" label={"Email"} />
+        }} placeholder="saketh@gmail.com" label={"Email"} />
         <InputBox onChange={(e) => {
           setPassword(e.target.value)
         }} placeholder="123456" label={"Password"} />
